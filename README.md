4eef7e340e0eb30794bc412b9444c06b7d87c15a
Mykhailo Kazakov <kamiournew@gmail.com>
2019-12-22 22:05:47 +0200

    completed REST API creation

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-"# NestjsAPI" 
+"# NestjsAPI"
+
+- POST request for creating a product can be tested by sending this JSON body via Postman, for example, to localhost:3001/products:
+{
+	"title": "Test title",
+	"description": "Test description",
+	"price": 29.99
+}
+
+- GET request for checking ALL the created products can be tested by sending a GET request to localhost:3001/products
+
+- GET request for checking ANY SINGLE created product can be tested by sending a GET request to localhost:3001/products/id, where you can take the ID of any product you created before
+
+- PATCH request to update a product can be tested by sending a PATCH request to localhost:3001/products/id with the body of the updated product, for example:
+{
+	"title": "Patched title"
+}
+
+- DELETE request to remove a single product can be tested by sending a DELETE request to localhost:3001/products/id
--- nestjs-test-project/src/products/products.controller.ts
+++ nestjs-test-project/src/products/products.controller.ts
@@ -1,13 +1,45 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Patch, Delete } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
-@Controller('/produts')
+@Controller('/products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  addProduct(@Body('title') prodTitle: string, @Body('description') prodDesc: string, @Body('price') prodPrice: number): any {
+  addProduct(
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number
+    ): any {
     const generatedId = this.productsService.insertProduct(prodTitle, prodDesc, prodPrice);
     return { id: generatedId };
   }
+
+  @Get()
+  getAllProducts() {
+    return this.productsService.getProducts();
+  }
+
+  @Get(':id')
+  getProduct(@Param('id') prodID: string) {
+    return this.productsService.getSingleProduct(prodID);
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id') prodID: string,
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+    ) {
+    this.productsService.updateProduct(prodID, prodTitle, prodDesc, prodPrice);
+    return null;
+  }
+
+  @Delete(':id')
+  removeProduct(@Param('id') prodID: string) {
+    this.productsService.removeSingleProduct(prodID);
+    return null;
+  }
+
 }
