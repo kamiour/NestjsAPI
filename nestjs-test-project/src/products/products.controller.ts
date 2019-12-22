ca406b55fe75002952856f50886cd42ce2ecc596
Mykhailo Kazakov <kamiournew@gmail.com>
2019-12-22 20:34:26 +0200

    first commit

---
+++ nestjs-test-project/src/products/products.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { ProductsService } from './products.service';
+
+@Controller('/produts')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  addProduct(@Body('title') prodTitle: string, @Body('description') prodDesc: string, @Body('price') prodPrice: number): any {
+    const generatedId = this.productsService.insertProduct(prodTitle, prodDesc, prodPrice);
+    return { id: generatedId };
+  }
+}
